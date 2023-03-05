b073f2295105d84cd86fb62e68dea0b91fa48252
lesayobruce <lesayobruce419@gmail.com>
2023-03-06 00:58:15 +0300

    updated App.js

--- my-app-frontend/src/App.js
+++ my-app-frontend/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom'
+import LandingPage from './components/LandingPage'
+import LoginPage from './components/LoginPage'
+import RegisterPage from './components/RegisterPage'
+import ForgetPasswordPage from './components/ForgetPasswordPage'
+import HomePage from './components/HomePage'
+import './App.css'
+export default function App() {
+    return (
+    <>
+        <Router>
+            <div>
+                <Routes>
+                    <Route path="/" element={ <LandingPage/> } />
+                    <Route path="/login" element={ <LoginPage/> } />
+                    <Route path="/register" element={ <RegisterPage />} />
+                    <Route path="/forget-password" element={ <ForgetPasswordPage />} />
+                    <Route path="/home" element={< HomePage/> } />
+            "
+                </Routes>
+            </div>
+        </Router>
+        </>
+    )
 }
-
-export default App;
