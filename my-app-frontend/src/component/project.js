aa16b308e4758dff63eca67f188c804d290866d4
lesayobruce <lesayobruce419@gmail.com>
2023-03-06 00:48:16 +0300

    created a project file

---
+++ my-app-frontend/src/component/project.js
@@ -0,0 +1,20 @@
+import React from 'react'
+function Project() {
+    return (
+      <div>
+        <nav className="nav">
+          <div className="nav-left">
+            <a className="brand" href="#">
+                Project Manager
+            </a>
+          </div>
+          <div className="nav-right">
+            <div className="tabs">
+              <a href>Project Management App</a>
+            </div>
+          </div>
+        </nav>
+      </div>
+    );
+}
+export default Project
